6a65016067e3aacf150dccc833c4c7577224ffff
Eduardo Backese <eduardo.programacao@hotmail.comm>
2023-04-16 23:56:08 -0300

    EBA

---
+++ script2.js
@@ -0,0 +1,14 @@
+function botao(){
+    var bottum = document.querySelector('.menu-mobile');
+    var menu = document.querySelector('.menu-itens');
+
+    if(menu.classList.contains('showMenu')){
+        menu.classList.remove('showMenu')}
+        else{ menu.classList.add('showMenu');}
+    
+    
+
+
+
+    
+}
